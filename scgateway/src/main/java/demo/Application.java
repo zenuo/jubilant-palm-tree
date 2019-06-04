c3f8fdadf37c6a9e7e418b0b3e025c5778a5f338
zenuo <zenuo@protonmail.com>
2019-06-04 21:21:31 +0800

    实现网关例子

---
+++ scgateway/src/main/java/demo/Application.java
@@ -0,0 +1,56 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * 说明
+ *
+ * @author zenuo
+ * @date 2019/06/04
+ */
+@RestController
+@EnableConfigurationProperties(UriConfiguration.class)
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("Hello", "World"))
+                        .uri(uriConfiguration.httpBin))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(uriConfiguration.httpBin))
+                .build();
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+
+}
+
+@ConfigurationProperties
+class UriConfiguration {
+    String httpBin = "http://httpbin.org:80";
+}
---
+++ scgateway/src/test/java/demo/ApplicationTest.java
@@ -0,0 +1,63 @@
+package demo;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * 说明
+ *
+ * @author zenuo
+ * @date 2019/06/04
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
+public class ApplicationTest {
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void contextLoads() throws Exception {
+        //Stubs
+        WireMock.stubFor(
+                WireMock.get(WireMock.urlEqualTo("/get"))
+                        .willReturn(WireMock.aResponse()
+                                .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                                .withHeader("Content-Type", "application/json")));
+
+        WireMock.stubFor(
+                WireMock.get(WireMock.urlEqualTo("/delay/3"))
+                        .willReturn(WireMock.aResponse()
+                                .withBody("no fallback")
+                                .withFixedDelay(3000)));
+
+        webTestClient.get()
+                .uri("/get")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("$.headers.Hello")
+                .isEqualTo("World");
+
+        webTestClient.get()
+                .uri("/delay/3")
+                .header("Host", "www.hystrix.com")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .consumeWith(
+                        response -> Assertions
+                                .assertThat(response.getResponseBody())
+                                .isEqualTo("fallback".getBytes()));
+    }
+}
