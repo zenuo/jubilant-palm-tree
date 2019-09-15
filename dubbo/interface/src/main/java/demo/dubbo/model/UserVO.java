bc7ab9420387ae6fca793e88b19217c239c8830d
zenuo <zenuo@protonmail.com>
2019-09-15 20:37:03 +0800

    添加UserService实现

--- dubbo/interface/src/main/java/demo/dubbo/model/UserVO.java
+++ dubbo/interface/src/main/java/demo/dubbo/model/UserVO.java
@@ -1,10 +1,12 @@
 package demo.dubbo.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@Builder
 public class UserVO implements Serializable {
 
     private Integer id;
